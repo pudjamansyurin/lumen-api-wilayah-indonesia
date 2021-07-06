887bb49fd91d9f59876514c181a03df49a7d2e85
Pudja Mansyurin <pujakusumae@hotmail.com>
2021-07-06 20:23:33 +0700

    Use laravel migration to insert .sql dump

--- www/database/migrations/2021_06_26_014119_create_wilayah_table.php
+++ www/database/migrations/2021_06_26_014119_create_wilayah_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateWilayahTable extends Migration
@@ -14,11 +15,16 @@ class CreateWilayahTable extends Migration
     public function up()
     {
         Schema::create('wilayah', function (Blueprint $table) {
-            $table->string('KODE_WILAYAH', 8)->nullable(false)->unique()->primary();
-            $table->string('MST_KODE_WILAYAH', 8)->nullable(false)->unique();
+            $table->string('KODE_WILAYAH', 8)->nullable(false)->primary();
+            $table->string('MST_KODE_WILAYAH', 8)->nullable(false);
             $table->string('NAMA', 255)->nullable(false);
             $table->enum('LEVEL', ['1', '2', '3', '4'])->nullable(false);
+            $table->unique(['KODE_WILAYAH', 'MST_KODE_WILAYAH']);
         });
+
+        $file = realpath(__DIR__ . '/wilayah-20170525.sql');
+
+        DB::unprepared(file_get_contents($file));
     }
 
     /**
